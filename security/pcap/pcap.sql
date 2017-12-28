10fbb0617ff85e3240643ed78413c55119f96a04
Jonathan <sifreed77@gmail.com>
2017-12-27 22:48:32 -0500

    Updated information on file

--- security/pcap/pcap.sql
+++ security/pcap/pcap.sql
@@ -1,5 +1,7 @@
+#Map location of the file
 `/test.pcap`
 
+#while the spark CLI is running, execute the create table command, like so:
 create table caps (src_ip, src_port, tcp_session, packet_length, data, src_mac_address, dst_port, type, dst_ip, dst_mac_address, network, TIMESTAMP) from `/test.pcap`;
 
 sqlline.bat -u "jdbc:drill:zk=local"
